170411b9468ebc8769ca9816828c3f7a1f199f4c
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2018-02-23 18:31:02 +0900

    Fix unscoped and reorder

--- lib/zomekip/sitebk/scanner.rb
+++ lib/zomekip/sitebk/scanner.rb
@@ -8,8 +8,9 @@ module Zomekip
 
       def scan_ids
         @models.each_with_object(HashWithIndifferentAccess.new) do |model, hash|
-          hash[model.table_name] = model.in_site(@site)
-                                        .order(model.primary_key)
+          hash[model.table_name] = model.unscoped
+                                        .in_site(@site)
+                                        .reorder(model.primary_key)
                                         .pluck(model.primary_key)
         end
       end
